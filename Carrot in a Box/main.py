212defbf7891755b8509f4d24a83ceda3de71608
conor <conorlynam1234@gmail.com>
2025-01-06 13:00:25 +0000

    Finished

--- Carrot in a Box/main.py
+++ Carrot in a Box/main.py
@@ -1,44 +1,44 @@
 import random
-
-RED_BOX = False
-GOLD_BOX = False
-
+import time
 
 def game_start():
-    global p1_name, p2_name, p1, p2
+    global p1_name, p2_name, p1, p2, r, RED_BOX, GOLD_BOX
     print('Welcome to Carrot in a Box!')
     p1_name = input('Enter your name: ')
     p2_name = input('Enter your name: ')
 
-    p1 = (RED_BOX, p1_name)
-    p2 = (GOLD_BOX, p2_name)
+    RED_BOX, GOLD_BOX = assign_carrot()
+    
+    p1 = [RED_BOX, p1_name]
+    p2 = [GOLD_BOX, p2_name]
 
     print(f'There are 2 boxes in front of you. One belongs to {p1_name} and one belongs to {p2_name}.')
     print('The game works by having 1 player look inside their box and see if they have the carrot or not. The other player must then decide whether or not they want to swap boxes')
-
-    assign_carrot()
     
     r = random.randint(0,1)
     if r == 0:
         print(f'{p1_name} goes first')
         print('Look inside your box...')
-        check_box(p1)
+        player_ready_to_check_box(p1)
     else:
         print(f'{p2_name} goes first')
         print('Look inside your box...')
-        check_box(p2)
-        
+        player_ready_to_check_box(p2)
     
-
+    decide_to_swap_boxes()
+    if True in p1:
+        print(f'{p1_name} has the carrot!!!\nGame over!!')
+        
 
 def assign_carrot():
-    r = random.randint(0,1)
-    if r == 0:
+    ran = random.randint(0,1)
+    if ran == 0:
         RED_BOX = True
         GOLD_BOX = False
     else:
-        GOLD_BOX = True
-        RED_BOX = False
+        GOLD_BOX = False
+        RED_BOX = True
+    return RED_BOX, GOLD_BOX
     
 
 def check_box(p):
@@ -52,5 +52,34 @@ def check_box(p):
             print(f'{p2_name}, you have the carrot!')
         else:
             print(f'{p2_name}, you do not have the carrot.')
+            
+def player_ready_to_check_box(p):
+    while True:
+        i = input('Player, are you ready to open the box. Make sure your oponent is not looking...\nY or N\n>')
+        if i.lower() == 'y':
+            check_box(p)
+            time.sleep(5)
+            print('\n' * 100)
+            break
+        elif i.lower() == 'n':
+            print('Alright, let\'s try again.')
+            
+
+def decide_to_swap_boxes():
+    if r == 0:
+        i = input(f'{p2_name}, do you want to swap the boxes?\nY or N\n>')
+        if i.lower() == 'y':
+            print('Swapping boxes')
+            p1[0], p2[0] = p2[0], p1[0]
+        else:
+            print('Not swapping boxes')
+    else:
+        i = input(f'{p1_name}, do you want to swap the boxes?\nY or N\n>')
+        if i.lower() == 'y':
+            print('Swapping boxes')
+            p1[0], p2[0] = p2[0], p1[0]
+        else:
+            print('Not swapping boxes')
+    
             
 game_start()
--- Carrot in a Box/test.py
+++
@@ -1,5 +0,0 @@
-x = True
-p = ('james', 'dog', 10, x)
-
-if True in p:
-    print('The name James is in the tuple')
